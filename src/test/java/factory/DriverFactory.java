75a650095c2349be6028b7e80d64b68404d21dc0
veroeb <vecheza@gmail.com>
2022-01-14 20:09:57 -0300

    Added a DriverFactory and Hooks

---
+++ src/test/java/factory/DriverFactory.java
@@ -0,0 +1,20 @@
+package factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    public static WebDriver driver;
+
+    public static WebDriver initializeDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+}
