b6ee746b158a680fa0cd98a74419e27b7537159d
veroeb <vecheza@gmail.com>
2022-01-14 19:46:39 -0300

    Added steps and basic structure

---
+++ src/test/java/steps/Steps.java
@@ -0,0 +1,38 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+    @Given("I access Aliexpress webpage")
+    public void accessAliexpressWebpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I search for {string} with the search bar")
+    public void searchWithSearchBar(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I can see all results")
+    public void seeResults() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the next search results page")
+    public void clickNextResultsPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the second card")
+    public void clickSecondCard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I can verify that there's at least one item available")
+    public void verifyOneItemAvailable() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ target/test-classes/aliexpress.feature
@@ -0,0 +1,12 @@
+#Veronica Echezarreta
+#01/14/2022
+
+Feature: Aliexpress
+
+  Scenario: Verify there's at least one iPhone available
+    Given I access Aliexpress webpage
+    When I search for "iPhone" with the search bar
+    Then I can see all results
+    When I click on the next search results page
+    And I click on the second card
+    Then I can verify that there's at least one item available
