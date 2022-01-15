a8326535c8885e352c812b02f9ab0939d5783ac3
veroeb <vecheza@gmail.com>
2022-01-14 21:04:32 -0300

    Added logic to the search bar

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,9 +1,31 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class HomePage extends BasePage{
+    //==============LOCATORS==================
+    @FindBy(id = "search-key")
+    private WebElement searchBar;
+
+    @FindBy(css = "#root div.product-container > div:nth-child(2) > a")
+    private List<WebElement> results;
+
+    @FindBy(css = ".next-pagination-pages > :nth-child(3)")
+    private WebElement nextPage;
+    //========================================
+
     public HomePage(WebDriver driver) {
         super(driver);
+    }
+
+    public void searchWithSearchBar(String phoneBrand){
+        getWait().until(ExpectedConditions.elementToBeClickable(searchBar)).click();
+        searchBar.sendKeys(phoneBrand);
+        searchBar.submit();
     }
 }
--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -13,15 +13,17 @@ public class Steps {
     @Given("I access Aliexpress webpage")
     public void accessAliexpressWebpage() {
         /**
-         * The user access the Aliexpress webpage
+         * Access the Aliexpress webpage
          */
         driver = DriverFactory.getDriver();
         new HomePage(driver).getUrl("https://aliexpress.com/");
     }
     @When("I search for {string} with the search bar")
-    public void searchWithSearchBar(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void searchWithSearchBar(String phoneBrand) {
+        /**
+         * Search a product in the search bar
+         */
+        new HomePage(driver).searchWithSearchBar(phoneBrand);
     }
     @Then("I can see all results")
     public void seeResults() {
